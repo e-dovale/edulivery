c8b2611ac9e6500c49206d1ee0887601a77213de
Eduardo do Vale <edu.dovale@gmail.com>
2025-07-20 18:12:20 -0300

    first commit

---
+++ src/main/java/com/evale/edulivery/EduliveryApplication.java
@@ -0,0 +1,13 @@
+package com.evale.edulivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evale/edulivery/EduliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evale.edulivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
